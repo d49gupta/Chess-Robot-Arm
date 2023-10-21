fbeef9de6a9ff7e51014db5589b882fe94583997
Dharma Gupta <dharma.anita@gmail.com>
2023-10-21 11:56:23 -0400

    Added Matlab visualizion program

---
+++ InverseKinematics.m
@@ -0,0 +1,76 @@
+dhparams = [0   	pi/2	0   	0;
+            0.4318	0       0       0
+            0.0203	-pi/2	0.15005	0;
+            0   	pi/2	0.4318	0;
+            0       -pi/2	0   	0;
+            0       0       0       0];
+
+% empty contatiner defining the strcuture and kinematics of the robot
+robot = rigidBodyTree; 
+
+% First link (revolute joint) and its associated dh parameters (first
+% row) used to specify the transformation matrix of the joint (relationship
+% between joint angle and its parent/child links. 
+body1 = rigidBody('body1');
+jnt1 = rigidBodyJoint('jnt1','revolute');
+setFixedTransform(jnt1,dhparams(1,:),'dh');
+
+% Attach joint to body and body to base of robot
+body1.Joint = jnt1;
+addBody(robot,body1,'base')
+
+% Repeat for all other links/joints
+body2 = rigidBody('body2');
+jnt2 = rigidBodyJoint('jnt2','revolute');
+body3 = rigidBody('body3');
+jnt3 = rigidBodyJoint('jnt3','revolute');
+body4 = rigidBody('body4');
+jnt4 = rigidBodyJoint('jnt4','revolute');
+body5 = rigidBody('body5');
+jnt5 = rigidBodyJoint('jnt5','revolute');
+body6 = rigidBody('body6');
+jnt6 = rigidBodyJoint('jnt6','revolute');
+
+setFixedTransform(jnt2,dhparams(2,:),'dh');
+setFixedTransform(jnt3,dhparams(3,:),'dh');
+setFixedTransform(jnt4,dhparams(4,:),'dh');
+setFixedTransform(jnt5,dhparams(5,:),'dh');
+setFixedTransform(jnt6,dhparams(6,:),'dh');
+
+body2.Joint = jnt2;
+body3.Joint = jnt3;
+body4.Joint = jnt4;
+body5.Joint = jnt5;
+body6.Joint = jnt6;
+
+addBody(robot,body2,'body1')
+addBody(robot,body3,'body2')
+addBody(robot,body4,'body3')
+addBody(robot,body5,'body4')
+addBody(robot,body6,'body5')
+
+% Initializes robot angles at random (provides initial guess)
+randConfig = robot.randomConfiguration;
+
+% Desired transformation matrix 
+x = input("Enter the desired x coordinate: ");
+y = input("Enter the desired y coordinate: ");
+z = input("Enter the desired z coordinate: ");
+user_tform = [1 0 0 x; 0 1 0 y; 0 0 -1 z; 0 0 0 1];
+tform = getTransform(robot,randConfig,"body6","body1");
+showdetails(robot)
+
+% Calculate the Inverse Kinematics
+ik = inverseKinematics("RigidBodyTree",robot);
+weights = [0.25 0.25 0.25 1 1 1];
+initialguess = homeConfiguration(robot);
+
+% Inverse kinematics solved for body6
+[configSoln,solnInfo] = ik("body6",user_tform,weights,initialguess);
+configSoln_degrees = rad2deg([configSoln.JointPosition]);
+show(robot,configSoln);
+
+% Display joint angles
+disp("Joint Angles:");
+disp(configSoln_degrees);
+
--- inverse_kinematics_visual.py
+++ inverse_kinematics_visual.py
@@ -1,45 +1,69 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import math
 
-joint_angles = [0.2, 0.3, 0.5, 0.7, 0.9, 1.2]
-link_lengths = [1, 2, 3, 1, 2, 1]
+def find_joint_angles(destination_point, link_lengths):
+    x = destination_point[0]
+    y = destination_point[1]
+    z = destination_point[2]
 
-destination_point = [3, 2, 1]
-origin = np.array([0, 0, 0])
-joint_positions = [origin]
+    theta1 = math.atan(x/y)
+    r = math.sqrt(pow(x,2)+ pow(y,2))
+    r_squared = pow(r, 2)
+    s = z - link_lengths[0]
+    s_squared = pow(s, 2)
 
-# Calculate joint positions based on the joint angles and link lengths
-for i in range(len(joint_angles)): # Adjust the math to calculate 3D point based off your robot
-    angle = joint_angles[i]
-    length = link_lengths[i]
-    x = joint_positions[-1][0] + length * np.cos(angle)
-    y = joint_positions[-1][1] + length * np.sin(angle)
-    z = joint_positions[-1][2] + length * np.sin(angle)  # Adjust for 3D
-    joint_positions.append(np.array([x, y, z]))
+    theta3 = math.acos((r_squared + s_squared - pow(link_lengths[1], 2) - pow(link_lengths[2], 2))/(r_squared + s_squared))
+    theta2 = math.asin(((link_lengths[1] + link_lengths[2]*math.cos(theta3))*s - link_lengths[2]*math.sin(theta3)*r)/(2*link_lengths[1]*link_lengths[2]))
+    theta4 = -1*math.asin(math.sin(theta1)*math.cos(theta2-theta3)/math.cos(theta1))
+    theta5 = math.acos(math.sin(theta1))
 
-# Create a 3D plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+    joint_angles = [theta1, theta2, theta3, theta4, theta5]
+    return joint_angles
+    
+def plot_graph(joint_angles, link_lengths, destination_point):
+    origin = np.array([0, 0, 0])
+    joint_positions = [origin]
 
-# Plot the links of the robot arm
-for i in range(1, len(joint_positions)):
-    x_values = [joint_positions[i - 1][0], joint_positions[i][0]]
-    y_values = [joint_positions[i - 1][1], joint_positions[i][1]]
-    z_values = [joint_positions[i - 1][2], joint_positions[i][2]]
-    ax.plot(x_values, y_values, z_values, 'b-o')
+    # Calculate joint positions based on the joint angles and link lengths
+    for i in range(len(joint_angles)): # Adjust the math to calculate 3D point based off your robot
+        angle = joint_angles[i]
+        length = link_lengths[i]
+        x = joint_positions[-1][0] + length * np.cos(angle)
+        y = joint_positions[-1][1] + length * np.sin(angle)
+        z = joint_positions[-1][2] + length * np.sin(angle)  # Adjust for 3D
+        joint_positions.append(np.array([x, y, z]))
 
-# Plot the comparison point and origin
-ax.scatter(origin[0], origin[1], origin[2], c='green', marker='o', label='Origin')
-ax.scatter(destination_point[0], destination_point[1], destination_point[2], c='red', marker='o', label='Comparison Point')
+    # Create a 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_title('Robot Arm Configuration in 3D')
+    # Plot the links of the robot arm
+    for i in range(1, len(joint_positions)):
+        x_values = [joint_positions[i - 1][0], joint_positions[i][0]]
+        y_values = [joint_positions[i - 1][1], joint_positions[i][1]]
+        z_values = [joint_positions[i - 1][2], joint_positions[i][2]]
+        ax.plot(x_values, y_values, z_values, 'b-o')
 
-# Add a legend
-ax.legend()
+    # Plot the comparison point and origin
+    ax.scatter(origin[0], origin[1], origin[2], c='green', marker='o', label='Origin')
+    ax.scatter(destination_point[0], destination_point[1], destination_point[2], c='red', marker='o', label='Comparison Point')
 
-# Show the plot
-plt.show()
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title('Robot Arm Configuration in 3D')
+    ax.legend()
+    plt.show()
+
+def main():
+    link_lengths = [4, 10, 12.5, 7.5, 3]
+    destination_point = [10, 10, 10]
+
+    joint_angles = find_joint_angles(destination_point, link_lengths)
+
+    plot_graph(joint_angles, link_lengths, destination_point)
+
+if __name__ == "__main__":
+    main()
